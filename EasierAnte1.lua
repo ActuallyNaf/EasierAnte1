75c25d7eba2759f35792e977b27d65cd4cca2686
ActuallyNaf <126985757+ActuallyNaf@users.noreply.github.com>
2025-04-30 15:12:23 +0300

    Added in-game description

---
+++ EasierAnte1.lua
@@ -0,0 +1,15 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Easier Ante 1
+--- MOD_ID: easierantle1
+--- MOD_AUTHOR: [Naf]
+--- MOD_DESCRIPTION: Removes eternal / perishable / rental jokers from shops and boosters packs on Ante 1 and below
+
+--- PRIORITY: -5
+--- DISPLAY_NAME: Easier Antle 1
+--- BADGE_COLOUR: d18017
+
+----------------------------------------------
+------------ MOD CODE ------------------------
+
+----------------------------------------------
+------------ END MOD CODE --------------------
